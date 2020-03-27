e195acac433840eff7ef068cea421b7597273523
Jamie <kookjc6@gmail.com>
2020-03-27 13:38:41 -0500

    added education section

--- react-portfolio/src/components/Intro/index.js
+++ react-portfolio/src/components/Intro/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./style.css";
 import headshot from "./headshot.jpg";
 import resume from "./ResumeKook.pdf"; 
+import Section from "../Section"; 
+import About from "../About"; 
 
-function Intro(props) {
+function Intro() {
   return (
     <div>
         <header id="home" className="row header align-items-center">
@@ -20,14 +22,9 @@ function Intro(props) {
             </div>
             <div className="col-sm-4 d-flex align-items-center justify-content-center"><img className="img-fluid headshot" src={headshot}/></div>
         </header>
-        <div className="section about row">
-            <div className="overlay d-flex align-items-center">
-                <div className="container">
-                    <p>Web Developer coming from a background in science education and research. Experienced in creating a <a href="http://www.surgeuniverse.com/" target="_blank">web-hosted educational game</a>  that used theory and data-driven feedback in its effectiveness and clear presentation of information. Recently earned a certificate in Full-Stack Web Development from Vanderbilt University, acquiring a functional knowledge of HTML, CSS, Javascript, React.js, Node.js, and more. A great collaborator and communicator, having used my organizational and critical thinking skills to move projects forward both as a leader and as a team member. A quick learner who is always eager to tackle difficult problems and enjoys puzzling through a new challenge. Looking forward to developing new web technologies, utilizing my background and technical skills, and adapting along the way.</p>
-                    <button className="btn btn-light"><a href="#portfolio">View my work</a></button>
-                </div>
-            </div>
-        </div>
+        <Section>
+            <About/>
+        </Section>
     </div>
   );
 }
--- react-portfolio/src/components/MainSection/index.js
+++ react-portfolio/src/components/MainSection/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./style.css";
 import Intro from "../Intro"; 
+import Section from "../Section"; 
+import Education from "../Education"; 
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function MainSection(props) {
   return (
-      <Intro/>
+      <div className="main">
+        <Intro/>
+        <Education/>
+      </div>
+      
   );
 }
 
