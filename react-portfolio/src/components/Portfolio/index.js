75819d4704f559106b4df62d3aa10fd572549ae2
Jamie <kookjc6@gmail.com>
2020-03-27 21:23:32 -0500

    added popup projroject information on click to learn more

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useRef} from "react";
 import "./style.css";
 import Section from "../Section"; 
 import Project from "../Project"; 
@@ -11,12 +11,14 @@ function Portfolio() {
     const [page2Projects, setPage2Projects]= useState(projectList2); 
     const [currentPage, setCurrentPage]= useState(1); 
     const [cssChange, setCssChange] = useState("row p-2 absolutePos"); 
-    const [projectsHidden, setProjectsHidden]= useState({}); 
+    const [projectListHidden, setProjectListHidden]= useState({}); 
+    const [projectInfoHidden, setProjectInfoHidden]= useState({display: "none"}); 
+    const [currentProject, setCurrentProject]= useState({}); 
   
-    const handleClick= ()=>{
+    const handleNextPageClick= ()=>{
         //had weird offsetting bug where position would not be centered. Had to make the computer rerender the position as absolute in order to go back to center. 
         setCssChange("row p-2 staticPos")
-        setProjectsHidden({display: "none"}); 
+        setProjectListHidden({display: "none"}); 
         if (currentPage ===1 ){
             setDisplayProjects(page2Projects);
             setCurrentPage(2);  
@@ -26,10 +28,20 @@ function Portfolio() {
         }
         const timer = setTimeout(() => {
             setCssChange("row p-2 absolutePos");
-            setProjectsHidden({}); 
+            setProjectListHidden({}); 
             console.log("changed position back"); 
           }, 1);
           return () => clearTimeout(timer);
+    }
+
+    const handleLearnMoreClick= (id)=>{
+       setProjectListHidden({display: "none"}); 
+       setProjectInfoHidden({}); 
+        for (const project of displayProjects){
+            if (project.id === id){ 
+                setCurrentProject(project); 
+            }
+        }
     }
 
   return (
@@ -37,9 +49,9 @@ function Portfolio() {
            <div id="portfolio">
             <h1>Portfolio</h1>
             <div id="port-desk">
-                <div className={cssChange} id="port-paper" style={projectsHidden}>
+                <div className={cssChange} id="port-paper" style={projectListHidden}>
                     <div className="col-2 d-flex align-items-center">
-                        <button id="newProjects" onClick= {handleClick} className="btn switch"><i className="fas fa-chevron-left"></i></button>
+                        <button id="newProjects" onClick= {handleNextPageClick} className="btn switch"><i className="fas fa-chevron-left"></i></button>
                     </div>
                     <div className="col-8 projectContainer">
                         {displayProjects.map(project => (
@@ -50,24 +62,25 @@ function Portfolio() {
                                 title ={project.title}
                                 icon= {project.icon}
                                 shortd= {project.shortd}
+                                handleLearnMoreClick = {handleLearnMoreClick}
                             />
                         ))}
                     </div>
                     <div className="col-2 d-flex align-items-center">
-                        <button id="oldProjects" onClick= {handleClick} className="btn switch"><i className="fas fa-chevron-right"></i></button>
+                        <button id="oldProjects" onClick= {handleNextPageClick} className="btn switch"><i className="fas fa-chevron-right"></i></button>
                     </div>
                     
                 </div>
-                <div id="port-more" hidden>
+                <div id="port-more" style={projectInfoHidden}>
                     <div id="port-cont" className="container p-5 text-left animated slideInUp">
-                        <h2 id="port-title"></h2>
-                        <p id="port-descript"></p>
-                        <p id="port-role"></p>
-                        <p id="port-tech"></p>
-                        <img className="img-fluid" id="port-img" src=""/>
+                        <h2 id="port-title">{currentProject.title}</h2>
+                        <p id="port-descript">{currentProject.descript}</p>
+                        <p id="port-role">{currentProject.role}</p>
+                        <p id="port-tech">{currentProject.tech}</p>
+                        <img className="img-fluid" id="port-img" src={currentProject.img}/>
                         <div className="text-center buttongroup m-5">
-                            <button className="btn btn-dark m-1"><a target="_blank" href="" id="port-linkapp">View the application</a></button>
-                            <button className="btn btn-dark m-1"><a target="_blank" href="" id="port-linkhub">View the repository</a></button>
+                            <button className="btn btn-dark m-1"><a target="_blank" href={currentProject.linkapp} id="port-linkapp">View the application</a></button>
+                            <button className="btn btn-dark m-1"><a target="_blank" href={currentProject.linkhub} id="port-linkhub">View the repository</a></button>
                             <button className="btn btn-dark m-1" id="back"><a href="#portfolio">Back to Portfolio</a></button>
                         </div>
                     </div>
